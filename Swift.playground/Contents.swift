4099d2f6e7a01c996fab329e1e78242dc0577be4
yanshi <yanshi_hu@hiersun.com>
2016-11-09 17:38:58 +0800

    HTML语言练习

---
+++ Swift.playground/Contents.swift
@@ -0,0 +1,11 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import Foundation
+var str = "Hello, playground"
+//swift中的宏定义
+let tableview = NSLocalizedString("func tableView(){print(1234)}", comment: "hello")
+
+tableview
+
+let million = 1_000_000
